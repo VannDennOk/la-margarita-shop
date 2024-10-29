e7bad29d90c733319804218944b57e327f5a0184
Vanina Denegri <denegri.vanina@gmail.com>
2024-10-29 01:42:53 -0300

    Crea main.js. Agrega enlaces en botones de la página de inicio (Ir a la tienda, Ramos, Decoración, Bodas, Ver todos los productos, Ver talleres, Más sobre nosotras, Ver más opiniones). Agrega sección de opiniones.

---
+++ main.js
@@ -0,0 +1,39 @@
+/*TODO ESTO ES PROVISORIO HASTA QUE VEAMOS BIEN JAVA EN CLASE!!!*/
+
+/*BOTONES DE ENLACES*/
+
+document.getElementById("btn_irProductos").addEventListener("click", function() {
+    window.location.href = "pages/productos.html";
+});
+
+document.getElementById("btn_irRamos").addEventListener("click", function() {
+    window.location.href = "pages/productos-ramos.html";
+});
+
+document.getElementById("btn_irDecoracion").addEventListener("click", function() {
+    window.location.href = "pages/productos-decoración.html";
+});
+
+document.getElementById("btn_irBodas").addEventListener("click", function() {
+    window.location.href = "pages/productos-bodas.html";
+});
+
+document.getElementById("btn_irProductos2").addEventListener("click", function() {
+    window.location.href = "pages/productos.html";
+});
+
+document.getElementById("btn_irTalleres").addEventListener("click", function() {
+    window.location.href = "pages/talleres.html";
+});
+
+document.getElementById("btn_irNosotras").addEventListener("click", function() {
+    window.location.href = "pages/nosotras.html";
+});
+
+document.getElementById("btn_irOpiniones").addEventListener("click", function() {
+    window.location.href = "pages/opiniones.html";
+});
+
+
+
+
