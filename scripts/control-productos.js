9ee1d9a276e2b4911501e71cfdba83aab007a300
Vanina Denegri <denegri.vanina@gmail.com>
2024-12-13 03:26:49 -0300

    Agrega creación dinámica de los cards de productos. Filtra por tipo, disponibilidad y producto destacado para que cada card vaya a su sección correspondiente.

---
+++ scripts/control-productos.js
@@ -0,0 +1,439 @@
+const productosdb = [
+    {
+        id: "r01",
+        nombre: "Gerbera plus",
+        tipo: "ramos",
+        precio: "60.000",
+        imagen: "../images/productos/ramos-001-00001.jpg",
+        alt: "ramo de flores gerberas",
+        disponible: true,
+        descuento: false
+    },
+    {
+        id: "r02",
+        nombre: "Primavera total",
+        tipo: "ramos",
+        precio: "50.000",
+        imagen: "../images/productos/ramos-001-00002.jpg",
+        alt: "ramo de flores variadas",
+        disponible: true,
+        descuento: false,
+        destacado: false
+    },
+    {
+        id: "r03",
+        nombre: "Premium | Rosas y más",
+        tipo: "ramos",
+        precio: "75.000",
+        imagen: "../images/productos/ramos-001-00003.jpg",
+        alt: "ramo de flores rosas",
+        disponible: true,
+        descuento: false,
+        destacado: true
+    },
+    {
+        id: "r04",
+        nombre: "Primavera especial",
+        tipo: "ramos",
+        precio: "50.000",
+        imagen: "../images/productos/ramos-001-00004.jpg",
+        alt: "ramo de flores variadas",
+        disponible: true,
+        descuento: false,
+        destacado: false
+    },
+    {
+        id: "r05",
+        nombre: "Variedad de rosas",
+        tipo: "ramos",
+        precio: "60.000",
+        imagen: "../images/productos/ramos-001-00005.jpg",
+        alt: "ramo de flores rosas",
+        disponible: true,
+        descuento: false,
+        destacado: false
+    },
+    {
+        id: "r06",
+        nombre: "Gerbera veraniega",
+        tipo: "ramos",
+        precio: "50.000",
+        imagen: "../images/productos/ramos-001-00006.jpg",
+        alt: "ramo de flores gerberas",
+        disponible: true,
+        descuento: false,
+        destacado: true
+    },
+    {
+        id: "r07",
+        nombre: "Gerbera Mimi",
+        tipo: "ramos",
+        precio: "40.000",
+        imagen: "../images/productos/ramos-001-00007.jpg",
+        alt: "ramo de flores gerberas",
+        disponible: true,
+        descuento: false,
+        destacado: false
+    },
+    {
+        id: "r08",
+        nombre: "Crisantemos y más",
+        tipo: "ramos",
+        precio: "40.000",
+        imagen: "../images/productos/ramos-001-00008.jpg",
+        alt: "ramo de flores crisantemos",
+        disponible: true,
+        descuento: false,
+        destacado: false
+    },
+    {
+        id: "r09",
+        nombre: "Caléndula otoñal",
+        tipo: "ramos",
+        precio: "60.000",
+        imagen: "../images/productos/ramos-001-00009.jpg",
+        alt: "ramo de flores caléndulas",
+        disponible: true,
+        descuento: false,
+        destacado: false
+    },
+    {
+        id: "r10",
+        nombre: "Simplemente gerbera",
+        tipo: "ramos",
+        precio: "50.000",
+        imagen: "../images/productos/ramos-001-00010.jpg",
+        alt: "ramo de flores gerberas",
+        disponible: true,
+        descuento: false,
+        destacado: false
+    },
+    {
+        id: "r11",
+        nombre: "Siempre rosas rojas",
+        tipo: "ramos",
+        precio: "50.000",
+        disponible: true,
+        imagen: "../images/productos/ramos-001-00011.jpg",
+        alt: "ramo de flores rosas rojas",
+        descuento: false,
+        destacado: true
+    },
+    {
+        id: "r12",
+        nombre: "Siempre fresias",
+        tipo: "ramos",
+        precio: "40.000",
+        imagen: "../images/productos/ramos-001-00012.jpg",
+        alt: "ramo de flores fresias",
+        disponible: true,
+        descuento: false,
+        destacado: true
+    },
+    {
+        id: "b01",
+        nombre: "Bodas | Rosas clásicas",
+        tipo: "bodas",
+        precio: "50.000",
+        imagen: "../images/productos/bodas-003-00001.jpg",
+        alt: "ramo de bodas rosas",
+        disponible: true,
+        descuento: false,
+        destacado: true
+    },
+
+    {
+        id: "b02",
+        nombre: "Bodas | Rosas Diamante",
+        tipo: "bodas",
+        precio: "70.000",
+        imagen: "../images/productos/bodas-003-00002.jpg",
+        alt: "ramo de bodas rosas con diamantes",
+        disponible: true,
+        descuento: false,
+        destacado: false
+    },
+    {
+        id: "b03",
+        nombre: "Bodas | Rosas campestres",
+        tipo: "bodas",
+        precio: "50.000",
+        imagen: "../images/productos/bodas-003-00003.jpg",
+        alt: "ramo de bodas con margaritas",
+        disponible: true,
+        descuento: false,
+        destacado: false
+    },
+    {
+        id: "b04",
+        nombre: "Bodas | Rosas bicolor",
+        tipo: "bodas",
+        precio: "60.000",
+        imagen: "../images/productos/bodas-003-00004.jpg",
+        alt: "ramo de bodas rosas de diferentes colores",
+        disponible: true,
+        descuento: false,
+        destacado: false
+    },
+    {
+        id: "b05",
+        nombre: "Bodas | Popurrí campestre",
+        tipo: "bodas",
+        precio: "40.000",
+        imagen: "../images/productos/bodas-003-00005.jpg",
+        alt: "ramo de bodas flores silvestres",
+        disponible: true,
+        descuento: false,
+        destacado: true
+    },
+    {
+        id: "b06",
+        nombre: "Bodas | Rosas de primavera",
+        tipo: "bodas",
+        precio: "60.000",
+        imagen: "../images/productos/bodas-003-00006.jpg",
+        alt: "ramo de bodas rosas de diferentes tonos",
+        disponible: true,
+        descuento: false,
+        destacado: false
+    },
+    {
+        id: "b07",
+        nombre: "Bodas | Rosas modernas",
+        tipo: "bodas",
+        precio: "50.000",
+        imagen: "../images/productos/bodas-003-00007.jpg",
+        alt: "ramo de bodas rosas con pelotitas",
+        disponible: true,
+        descuento: false,
+        destacado: false
+    },
+    {
+        id: "b08",
+        nombre: "Bodas | Margaritas plus",
+        tipo: "bodas",
+        precio: "40.000",
+        imagen: "../images/productos/bodas-003-00008.jpg",
+        alt: "ramo de bodas con margaritas",
+        disponible: true,
+        descuento: false,
+        destacado: false
+    },
+    {
+        id: "b09",
+        nombre: "Bodas | Variedad mini",
+        tipo: "bodas",
+        precio: "30.000",
+        imagen: "../images/productos/bodas-003-00009.jpg",
+        alt: "ramo de bodas flores variadas y chicas",
+        disponible: true,
+        descuento: false,
+        destacado: false
+    },
+    {
+        id: "b10",
+        nombre: "Bodas | Variedad rosal",
+        tipo: "bodas",
+        precio: "50.000",
+        imagen: "../images/productos/bodas-003-00010.jpg",
+        alt: "ramo de bodas rosas variadas",
+        disponible: true,
+        descuento: false,
+        destacado: false
+    },
+    {
+        id: "b11",
+        nombre: "Bodas | Rosas coloridas",
+        tipo: "bodas",
+        precio: "70.000",
+        imagen: "../images/productos/bodas-003-00011.jpg",
+        alt: "ramo de bodas rosas variadas",
+        disponible: true,
+        descuento: false,
+        destacado: false
+    },
+    {
+        id: "b12",
+        nombre: "Bodas | Variedad plus",
+        tipo: "bodas",
+        precio: "50.000",
+        imagen: "../images/productos/bodas-003-00012.jpg",
+        alt: "ramo de bodas con flores variadas",
+        disponible: true,
+        descuento: false,
+        destacado: false
+    },
+    {
+        id: "d01",
+        nombre: "Jarrón pompom",
+        tipo: "decoracion",
+        precio: "75.000",
+        imagen: "../images/productos/decoracion-002-00001.jpg",
+        alt: "jarrón cilindro transparente con flores",
+        disponible: true,
+        descuento: false,
+        destacado: false
+    },
+    {
+        id: "d02",
+        nombre: "Jarrón primaveral",
+        tipo: "decoracion",
+        precio: "75.000",
+        imagen: "../images/productos/decoracion-002-00002.jpg",
+        alt: "jarrón redondo transparente con flores",
+        disponible: true,
+        descuento: false,
+        destacado: true
+    },
+    {
+        id: "d03",
+        nombre: "Jarrón rosas y fresias",
+        tipo: "decoracion",
+        precio: "85.000",
+        imagen: "../images/productos/decoracion-002-00003.jpg",
+        alt: "jarrón alargado transparente con flores",
+        disponible: true,
+        descuento: false,
+        destacado: false
+    },
+    {
+        id: "taller0001",
+        nombre: "Taller inicial",
+        tipo: "talleres",
+        precio: "40.000",
+        imagen: "../images/productos/talleres-004-00001.png",
+        alt: "flayer taller inicial",
+        disponible: true,
+        descuento: false,
+        destacado: true
+    }
+]
+
+/* function crearCard() {
+    let productosHTML = "";
+
+
+    for (let indice = 0; indice < productosdb.length; indice++) {
+        if (productosdb[indice].destacado) {
+            productosHTML += // es lo mismo que decir productosHTML = productosHTML++ 
+                ` 
+        <li class="container__products-card">
+            <div class="container__img_product">
+                <img class="img_product" src=${productosdb[indice].imagen}>
+            </div>
+            <div class="container__product-cards-info">
+                <div class="cards-info-title">
+                <h4 class="txt-body txt-small txt-dark txt-strong">${productosdb[indice].nombre}</h4>
+                <button class="btn__heart" type="button" aria-label="Botón me gusta">
+                    <i class="bi bi-heart bi-dark bi-small bi-hover"></i>
+                    <i class="bi bi-heart-fill bi-dark bi-small bi-hover"></i>
+                </button>
+            </div>
+                <p class="txt-body txt-small txt-dark">Desde: $ ${productosdb[indice].precio}</p>
+                <button class="btn-full btn-agregar-carrito btn-full-hover" aria-label="Agregar al carrito">Agregar al carrito</button>
+            </div>
+        </li>
+        `;
+        }
+    }
+
+    const contenedorProductos = document.getElementById("contenedor-productos-destacados");
+    contenedorProductos.innerHTML = productosHTML;
+
+}
+
+crearCard(); */
+
+
+
+
+//for (let indice = 0; indice < productosdb.length; indice++) {
+//if (productosdb[indice].disponible === true) {
+//    console.log(productosdb[indice].nombre + " está en stock");
+//} else {
+//    console.log(productosdb[indice].nombre + " no está en stock");
+//}
+//}
+
+//for (let indice = 0; indice < productosdb.length; indice++) {
+//    if (productosdb[indice].destacado === true) {
+//        console.log(productosdb[indice].nombre + " es un producto destacado");
+//        crearCard();
+//}
+
+//function crearCardsPorFiltro() {
+// Variables para almacenar los productos filtrados
+        let productosRamosHTML = "";
+        let productosBodasHTML = "";
+        let productosDecoracionHTML = "";
+        let productosTalleresHTML = "";
+        let productosDestacadosHTML = "";
+        let productosDisponiblesHTML = ""; // Para productos disponibles
+    
+        // Recorrer los productos y clasificarlos según su tipo, destacado o disponibilidad
+        for (let indice = 0; indice < productosdb.length; indice++) {
+            const producto = productosdb[indice];
+            const productoHTML = `
+                <li class="container__products-card">
+                    <div class="container__img_product">
+                        <img class="img_product" src=${producto.imagen} alt="${producto.alt}">
+                    </div>
+                    <div class="container__product-cards-info">
+                        <div class="cards-info-title">
+                            <h4 class="txt-body txt-small txt-dark txt-strong">${producto.nombre}</h4>
+                            <button class="btn__heart" type="button" aria-label="Botón me gusta">
+                                <i class="bi bi-heart bi-dark bi-small bi-hover"></i>
+                                <i class="bi bi-heart-fill bi-dark bi-small bi-hover"></i>
+                            </button>
+                        </div>
+                        <p class="txt-body txt-small txt-dark">Desde: $ ${producto.precio}</p>
+                        <button class="btn-full btn-agregar-carrito btn-full-hover" aria-label="Agregar al carrito">Agregar al carrito</button>
+                    </div>
+                </li>
+            `;
+    
+            // Clasificar según tipo
+            if (producto.tipo === "ramos") {
+                productosRamosHTML += productoHTML;
+            } else if (producto.tipo === "bodas") {
+                productosBodasHTML += productoHTML;
+            } else if (producto.tipo === "decoracion") {
+                productosDecoracionHTML += productoHTML;
+            } else if (producto.tipo === "talleres") {
+                productosTalleresHTML += productoHTML;
+            }
+    
+            // Clasificar según destacado
+            if (producto.destacado) {
+                productosDestacadosHTML += productoHTML;
+            }
+    
+            // Clasificar según disponibilidad
+            if (producto.disponible) {
+                productosDisponiblesHTML += productoHTML;
+            }
+        }
+    
+        // Insertar productos en sus contenedores correspondientes
+        if (document.getElementById("contenedor-productos-ramos")) {
+            document.getElementById("contenedor-productos-ramos").innerHTML = productosRamosHTML;
+        }
+        if (document.getElementById("contenedor-productos-bodas")) {
+            document.getElementById("contenedor-productos-bodas").innerHTML = productosBodasHTML;
+        }
+        if (document.getElementById("contenedor-productos-decoracion")) {
+            document.getElementById("contenedor-productos-decoracion").innerHTML = productosDecoracionHTML;
+        }
+        if (document.getElementById("contenedor-productos-talleres")) {
+            document.getElementById("contenedor-productos-talleres").innerHTML = productosTalleresHTML;
+        }
+        if (document.getElementById("contenedor-productos-destacados")) {
+            document.getElementById("contenedor-productos-destacados").innerHTML = productosDestacadosHTML;
+        }
+        if (document.getElementById("contenedor-productos-disponibles")) {
+            document.getElementById("contenedor-productos-disponibles").innerHTML = productosDisponiblesHTML;
+        }
+    
+//}
+
+//crearCardsPorFiltro();
